28c263524961c215bce3dd7c0c82c47708f3f642
yogeshmahawar <yogeshmahawar93@gmail.com>
2018-04-27 09:52:16 +0530

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -108,7 +108,7 @@ class FG_eval {
 MPC::MPC() {}
 MPC::~MPC() {}
 
-void MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
+void MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
   bool ok = true;
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
